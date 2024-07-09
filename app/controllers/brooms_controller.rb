43e0edccd42cd9b29f1266f6b985768d7371c8b9
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-09 16:59:26 +0200

    Updt model broom & controller

--- app/controllers/brooms_controller.rb
+++ app/controllers/brooms_controller.rb
@@ -1,13 +1,30 @@
 class BroomsController < ApplicationController
   def index
-  end
-
-  def new
+    @brooms = Broom.all
   end
 
   def show
+    @broom = Broom.find(params[:id])
+    @broom = Broom.new
+  end
+
+  def new
+    @broom = Broom.new
   end
 
   def create
+    @broom = Broom.new(broom_params)
+    @broom.user = current_user
+    if @broom.save
+      redirect_to brooms_path, notice: 'Broom was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def broom_params
+    params.require(:broom).permit(:name, :description, :photo, :price)
   end
 end
--- app/models/broom.rb
+++ app/models/broom.rb
@@ -1,3 +1,7 @@
 class Broom < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
 end
