ee4b5ae814b5c8cd8ea6f4cecdf5a99566d3da2f
Yannis Gaspard <yannis.gaspard@gmail.com>
2024-07-09 15:22:06 +0200

    did the brooms controller methods

--- app/controllers/brooms_controller.rb
+++ app/controllers/brooms_controller.rb
@@ -1,13 +1,29 @@
 class BroomsController < ApplicationController
   def index
-  end
-
-  def new
+    @brooms = Broom.all
   end
 
   def show
+    @broom = Broom.find(params[:id])
+    @broom = Broom.new
+  end
+
+  def new
+    @broom = Broom.new
   end
 
   def create
+    @broom = Broom.new(broom_params)
+    if @broom.save
+      redirect_to broom_path(@broom)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def broom_params
+    params.require(:broom).permit(:name, :description, :photo, :price)
   end
 end
