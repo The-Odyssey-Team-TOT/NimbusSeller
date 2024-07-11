3572b759ca4963baa9037c2e5c0d84553ecbc42c
Yannis Gaspard <yannis.gaspard@gmail.com>
2024-07-11 17:31:57 +0200

    fixed a lot of things

--- app/controllers/brooms_controller.rb
+++ app/controllers/brooms_controller.rb
@@ -2,6 +2,15 @@ class BroomsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
     @brooms = Broom.all
+    @markers = @brooms.geocoded.map do |broom|
+      {
+        lat: broom.latitude,
+        lng: broom.longitude
+      }
+    end
+    if params[:query].present?
+      @brooms = Broom.search_by_name_and_description(params[:query])
+    end
   end
 
   def show
@@ -23,9 +32,15 @@ class BroomsController < ApplicationController
     end
   end
 
+  def destroy
+    @broom = Broom.find(params[:id])
+    @broom.destroy
+    redirect_to brooms_path
+  end
+
   private
 
   def broom_params
-    params.require(:broom).permit(:name, :description, :photo, :price)
+    params.require(:broom).permit(:name, :description, :photo, :price, :address)
   end
 end
